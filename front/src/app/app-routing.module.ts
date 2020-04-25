f099d572643ab5f6e4d5610d9cf1700281b2dae9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-25 16:32:26 +0200

    css

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { DocComponent } from './routes/doc/doc.component';
+import { MenuComponent } from './routes/menu/menu.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
   { path: 'doc', component: DocComponent },
 ];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { FranceMapComponent } from './dataviz/france-map/france-map.component';
 import { HomeComponent } from './routes/home/home.component';
 import { DocComponent } from './routes/doc/doc.component';
+import { MenuComponent } from './routes/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, FranceMapComponent, HomeComponent, DocComponent],
+  declarations: [AppComponent, FranceMapComponent, HomeComponent, DocComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
