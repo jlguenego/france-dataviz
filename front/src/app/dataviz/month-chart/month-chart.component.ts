cd6a0160690613ad4e9de6ff2421cee76b484437
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-01 14:13:38 +0200

    ok

--- front/src/app/dataviz/month-chart/month-chart.component.ts
+++ front/src/app/dataviz/month-chart/month-chart.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  ElementRef,
+} from '@angular/core';
 import { Csv } from 'src/app/csv';
 import { Moment } from 'moment';
 import * as moment from 'moment';
+import * as d3 from 'd3';
 
 interface IntervalStruct {
   width: string;
@@ -26,7 +33,10 @@ export class MonthChartComponent implements OnInit {
   years: IntervalStruct[];
 
   begin: Moment;
-  constructor() {}
+  scale: number;
+  color = 'blue';
+
+  constructor(private elt: ElementRef) {}
 
   getMonthInitial(m: Moment): string {
     return m.format('M');
@@ -34,6 +44,9 @@ export class MonthChartComponent implements OnInit {
 
   ngOnInit(): void {
     this.buildCalendar();
+    this.setScale();
+    this.refresh();
+    this.title = this.csv.getCommandValue('title');
   }
 
   buildCalendar(): void {
@@ -63,5 +76,64 @@ export class MonthChartComponent implements OnInit {
       width: `${(end.month() + 1) * 2}em`,
       label: iday.format('YYYY'),
     };
+  }
+
+  setScale() {
+    const maxHeight = 20;
+    ((this.elt.nativeElement as Element).querySelector(
+      '.bars'
+    ) as HTMLElement).style.height = `${maxHeight + 2}em`;
+
+    const maxValue = Math.max(...this.csv.data.map((d) => +d.value));
+    this.scale = maxHeight / maxValue;
+  }
+
+  refresh() {
+    const bars = d3.select(
+      (this.elt.nativeElement as Element).querySelector('.bars')
+    );
+
+    const update = () => {
+      const feature = bars.selectAll('div.bar').data(this.csv.data);
+
+      const transform = (d: d3.DSVRowString<string>) => {
+        const months = Math.round(
+          moment.duration(moment(d.month, FORMAT).diff(this.begin)).asMonths()
+        );
+        return `translateX(${months * 2}em)`;
+      };
+
+      feature
+        .enter()
+        .append('div')
+        .classed('bar', true)
+        .attr('title', (d) => d.value)
+        .style('background-color', (d) => d.color || this.color)
+        .style('height', (d) => {
+          console.log('d: ', d);
+          const value = Math.round(+d.value * this.scale * 1000) / 1000;
+          const result = value + 'em';
+          console.log('result: ', result);
+          return result;
+        })
+        .on('mouseenter', (d, i, nodes) => {
+          this.label = d.value;
+          // d3.select(nodes[i]).classed('hovered', true);
+        })
+        .on('mouseleave', (d, i, nodes) => {
+          this.label = '';
+          // d3.select(nodes[i]).classed('hovered', false);
+        })
+        .on('touchstart', (d, i, array) => {
+          this.label = d.value;
+        })
+        .style('transform', transform);
+
+      feature.exit().remove();
+
+      feature.style('transform', transform);
+    };
+
+    update();
   }
 }
